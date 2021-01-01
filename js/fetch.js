4a7cd55394f145ed0a86488b3f2bb74245675355
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-01-01 17:15:24 +0100

    Adds craft info to astronauts profiles

--- js/fetch.js
+++ js/fetch.js
@@ -5,8 +5,12 @@ const btn = document.querySelector('button');
 
 function getProfiles(json) {
     const profiles = json.people.map(person => {
+        const craft = person.craft;
         return fetch(wikiUrl + person.name)
             .then( response => response.json() )
+            .then( profile => {
+                return {...profile, craft}
+            })
             .catch( err => console.log('Error fetching WIKI',err))
     });
     return Promise.all(profiles);
@@ -18,6 +22,7 @@ function generateHTML(data) {
         peopleList.appendChild(section);
         section.innerHTML = `
     <img src=${person.thumbnail.source}>
+    <span>${person.craft}</span>
     <h2>${person.title}</h2>
     <p>${person.description}</p>
     <p>${person.extract}</p>
