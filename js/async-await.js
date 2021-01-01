9a19805b5af12e2c97a41c0d8826d00ab2425b60
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-01-01 18:45:40 +0100

    Adds error handler

--- js/async-await.js
+++ js/async-await.js
@@ -4,14 +4,21 @@ const peopleList = document.getElementById('people');
 const btn = document.querySelector('button');
 
 // Handle all fetch requests
+async function getJSON(url) {
+  try {
+    const response = await fetch(url);
+    return await response.json();
+  } catch (error) {
+    throw error;
+  }
+}
+
  async function getPeopleInSpace(url) {
-   const peopleResponse = await fetch(url);
-   const peopleJSON = await peopleResponse.json();
+   const peopleJSON = await getJSON(url);
 
    const profiles = peopleJSON.people.map( async person => {
      const craft = person.craft;
-     const profileResponse = await fetch(wikiUrl + person.name);
-     const profileJSON = await profileResponse.json();
+     const profileJSON = await getJSON(wikiUrl + person.name);
 
      return {...profileJSON, craft};
    });
@@ -25,7 +32,7 @@ function generateHTML(data) {
     const section = document.createElement('section');
     peopleList.appendChild(section);
     section.innerHTML = `
-      <img src=${person.thumbnail.source}>
+      <img src=${person.thumbnail.source} alt=''>
       <span>${person.craft}</span>
       <h2>${person.title}</h2>
       <p>${person.description}</p>
@@ -39,5 +46,9 @@ btn.addEventListener('click',  (event) => {
 
   getPeopleInSpace(astrosUrl)
     .then (generateHTML)
+    .catch( e => {
+      peopleList.innerHTML = '<h3>Something went wrong</h3>';
+      console.error(e);
+    })
     .finally(() => event.target.remove())
 });
